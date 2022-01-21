d399e79717014ab6457d3226916c7d81370a9e5d
onowdev <onowdev@gmail.com>
2022-01-21 15:02:34 +0700

    create first

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class HelloWorld{
+    public static void main (String []args){
+
+        System.out.println("HelloWorld");
+        System.out.println("Hallo Java");
+        
+
+    }
+}
